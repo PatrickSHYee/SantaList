4ef4abdcea032f8df2dc8543528341e22682ed58
Patrick Yee <g0th1cazn@gmail.com>
2015-12-15 21:39:22 -0700

    Suppose to study for an interview....
    I wanted to study for an interview, but I went on to do this.  I guess I
    am studying because it is the same knowledge..  First I want to make a
    single user application before I committed to a multi-user application.

---
+++ SantaList/Controllers/ListController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SantaList.Controllers
+{
+    public class ListController : Controller
+    {
+        // GET: LIst
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Gift_Wish()
+        {
+            return View();
+        }
+    }
+}
